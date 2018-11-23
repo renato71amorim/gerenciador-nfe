1891aa51739f1111a1a713e7cd2e92e5f22c7bc4
Masaki Kawabata Neto <kawabata@usp.br>
2018-11-23 15:07:24 -0200

    refatorado para cli

--- tools/reparseHeaders.php
+++ tools/reparseHeaders.php
@@ -1,24 +1,31 @@
 <?php
-require '../app/app.php';
+// para ser rodado na linha de comando
+if ('cli' != PHP_SAPI) {
+    die('Não está na linha de comando');
+}
 
-use \Uspdev\Popper\Popper;
-$popper = new Popper($cfg);
+require '../app/app.php';
 
 // foi usado para corrigir unidades que vieram como null por erro no código
 
-//$emails = $popper->getAll();
-use \RedBeanPHP\R as R;
-$emails = R::exec("SELECT id, unidade, raw_header FROM nfeemail WHERE unidade='NONE' ORDER BY id DESC;");
-echo 'inicio';
-foreach ($emails as $email) {
+// aqui não usamos o popper paraque o redbean possa não trazer o raw_body
+// que é grande e estoura a memória
+//$emails = R::exec("SELECT id, unidade, raw_header FROM nfeemail WHERE unidade='NONE' ORDER BY id DESC;");
+
+$emails = $popper->findCollectionByYear();
+
+echo 'inicio' . PHP_EOL;
+while ($email = $emails->next()) {
     $header = imap_rfc822_parse_headers($email->raw_header);
     $unidade = $popper->getUnidade($header);
+    
     if ($unidade != $email->unidade) {
+        echo 'id: ' . $email->id . ';';
         $email->unidade = $unidade;
         $popper->storeEmail($email);
         $ret = $popper->parseEmail($email);
-        echo 'Parser: '. json_encode($ret).'<br>';
-        echo 'Unidade alterada email_id '.$email->id.'<br>';
+        echo 'Parser: ' . json_encode($ret) . PHP_EOL;
+        echo 'Unidade alterada email_id ' . $email->id . PHP_EOL;
     }
 }
 
