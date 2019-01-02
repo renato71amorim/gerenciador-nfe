baf8a67b497f56a005f00ea056237c3fa80048c9
Masaki Kawabata Neto <kawabata@usp.br>
2019-01-02 13:54:46 -0200

    implementado filtro por ano e unidade

--- www/emails.php
+++ www/emails.php
@@ -3,27 +3,34 @@ require '../app/app.php';
 
 echo 'Uso tabela email ' . $popper->usoDB() . 'MB';
 #echo '<br>';
+
+
+$unidade = empty($_GET['unidade']) ? '%' : $_GET['unidade'];
+$ano = empty($_GET['ano']) ? date('Y') : $_GET['ano'];
+
+
 echo ' | Anos: ';
-foreach ($popper->anos() as $ano) {
-    echo '<a href=?ano=' . $ano . '>' . $ano . '</a> | ';
+foreach ($popper->anos() as $a) {
+    echo '<a href=?ano=' . $a . '&unidade='.$unidade.'>' . $a . '</a> : ';
 }
+echo '<a href=?ano=%&unidade='.$unidade.'>TODOS</a>';
 #echo '<br>';
 
 echo ' | Unidades: ';
 foreach ($popper->unidades() as $u) {
-    echo '<a href=?ano=' . $u . '>' . $u . '</a> | ';
+    echo '<a href=?ano='.$ano.'&unidade=' . $u . '>' . $u . '</a> : ';
 }
+echo '<a href=?ano='.$ano.'&unidade=%>TODOS</a>';
 echo '<br>';
 
 echo '<br>';
 
-$ano = empty($_GET['ano']) ? date('Y') : $_GET['ano'];
+$popper->unidade = $unidade;
+$popper->ano = $ano;
+$emails = $popper->findCollection();
 
-$popper->unidade = '%';
-$popper->ano = '%';
-$emails = $popper->findCollection($ano);
-
-echo 'Filtros: Unidade: '.$popper->unidade;
+echo 'Filtros:';
+echo ' Unidade: '.$popper->unidade;
 
 echo ' | Ano: ' . $popper->ano . '<br>';
 //echo 'Total: ' . count($emails) . '<br>';
