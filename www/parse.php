48ca05e8a40dffa351707b57b85d9753eb5e1971
Masaki Kawabata Neto <kawabata@usp.br>
2019-01-31 10:49:35 -0200

    verifica e corrige unidade

--- www/parse.php
+++ www/parse.php
@@ -9,6 +9,21 @@ $id = $_GET['id'];
 
 $email = $popper->load($id);
 
+// vamos imprimir algumas coisas na tela
+echo 'id ' . $id . '<br>';
+echo 'unidade ' . $email->unidade;
+
+if ($email->unidade == 'NONE') {
+    $unidade_reparse = $popper->getUnidadeFromHeader2($email->raw_header);
+    if ($unidade_reparse != 'NONE') {
+        $email->unidade = $unidade_reparse;
+        Database::store($email);
+        echo ' -> '.$unidade_reparse;
+    }
+}
+
+echo '<br>';
+
 $ret = $popper->parseEmail($email);
 echo 'Parser (anexos, nfe exist, nfe novos): ' . json_encode($ret) . '<br>';
 
